1201ff1cfced91b516deba30363632be5dcbd8cd
Mauricio Soares <m.soares2603@gmail.com>
2015-01-24 17:01:18 -0200

    undo wrong stuff in index, update room.js configuration

--- js/app/room.js
+++ js/app/room.js
@@ -1,8 +1,12 @@
 (function(room, $) {
 
-  var defaults = {};
+  var defaults = {
+    editor: {};
+  };
 
   function Room(options) {
+    this.options = options;
+
     this.prepare();
   }
 
@@ -12,7 +16,7 @@
   };
 
   Room.prototype.configEditor = function() {
-
+    var editor = ace.edit(this.options.editor.el);
   };
 
 } (window.App, jQuery));
