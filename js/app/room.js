86950e98dee88f183cea8fd4560591c9c4d86f7f
Mauricio Soares <m.soares2603@gmail.com>
2015-01-24 16:58:01 -0200

    start editor configuration

--- js/app/room.js
+++ js/app/room.js
@@ -3,7 +3,16 @@
   var defaults = {};
 
   function Room(options) {
-
+    this.prepare();
   }
+
+  Room.prototype.prepare = function() {
+
+    this.configEditor();
+  };
+
+  Room.prototype.configEditor = function() {
+
+  };
 
 } (window.App, jQuery));
