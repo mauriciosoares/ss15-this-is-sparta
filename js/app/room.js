ccacf89e4c2d0e801f94221e9a15610563fe855e
Mauricio Soares <m.soares2603@gmail.com>
2015-01-24 17:19:14 -0200

    add event keyup to editor

--- js/app/room.js
+++ js/app/room.js
@@ -13,9 +13,21 @@
 
   Room.prototype.prepare = function() {
     this.room = this.getRoom();
-    this.fb = new Firebase(this.options.conn + this.room);
+    // this.fb = new Firebase(this.options.conn + this.room);
 
     this.configEditor();
+    this.aceInput = document.querySelector(this.options.editor.input);
+
+    this.bind();
+
+  };
+
+  Room.prototype.bind = function() {
+    this.aceInput.addEventListener('keyup', this.onAceKeyup.bind(this));
+  };
+
+  Room.prototype.onAceKeyup = function() {
+    console.log(this.editor.getValue());
   };
 
   Room.prototype.configEditor = function() {
